3c88683791911354ac094ee93a312aaf7c603ba7
ansonfoong <afoong@albany.edu>
2019-06-16 10:38:27 -0400

    not able to modify global var

--- clj-test/src/clj_test/core.clj
+++ clj-test/src/clj_test/core.clj
@@ -3,12 +3,21 @@
   (:require [clojure.string :as str])
 )
 
+(def MY_NAME "ANSON FOONG") ; declare global variable STRING
+
 (defn greeting [name] (str "Hello " name)) ; returns a string
 (def getAge (fn [age] (str "Age: " age))) ; returns age
 (defn someFunction [p, s] (println "I am some function") (println p) (println s))
 
 (defn callme [phone, state]
   (apply someFunction phone state)
+)
+
+(defn tempFunc [] (
+  let [MY_NAME "JACK RABBIT"]
+    (println MY_NAME)
+  )
+  (println MY_NAME)
 )
 
 (defn -main
@@ -23,4 +32,5 @@
   (def myage (getAge 18)) ; declare a variable called myage and set it equal to getAge()
   (println myage)
   (callme "1" "2")
+  (tempFunc)
 )
