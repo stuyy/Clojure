832ac6359f14b15e5959af47f8fcecd5728e4080
ansonfoong <afoong@albany.edu>
2019-06-29 19:29:52 -0400

    getInfo returns a formatted string with 3 parameters

--- src/clj_test/ch2.clj
+++ src/clj_test/ch2.clj
@@ -1,7 +1,7 @@
 (ns clj-test.ch2
     (:gen-class)
 )
-
+(defn getInfo [name age gender] (str (format "Name: %s\nAge: %s\nGender: %s\n" name age gender)))
 (defn -main "Main!" [& args]
     ; def can be used to define global symbols, these symbols are known as 'vars' or var objects.
     (def myName "Anson Foong")
@@ -9,10 +9,12 @@
     (def myGender "Male")
 
     (println (format "Name: %s\nAge: %s\nGender: %s\n" myName myAge myGender))
-    
+
     ; if we want to bind a symbol without changing it's value globally, use 'let'
     (let [myName "Jack" myAge 22 myGender "Male"]
         (println (format "Name: %s\nAge: %s\nGender: %s\n" myName myAge myGender))
     )
     (println (format "Name: %s\nAge: %s\nGender: %s\n" myName myAge myGender))
+    ; Call getInfo
+    (println (str "Get Info\n" (getInfo myName myAge myGender)))
 )
