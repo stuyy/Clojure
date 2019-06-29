f91650e3c9b3a0d850425ac18f13c58e58faaf9a
ansonfoong <afoong@albany.edu>
2019-06-29 18:17:16 -0400

    playing around with Clojure's Map Collection

--- src/clj_test/mylists.clj
+++ src/clj_test/mylists.clj
@@ -19,5 +19,21 @@
     ; Get the Count of the nameVector Collection
     (def sizeOfNames (count nameVector))
     (println (str "Total Names in Vector nameVector: " sizeOfNames))
+    ; The conj function adds elements to a data structure in the most natural way.
+    ; For Vectors, it'll add them at the end.
+    ; For Lists, conj will add items to the front of the list.
+    (conj nameVector "Matthew")
+    (println nameVector)
+
+    ; Maps
+    (def names {:anson 21 :lisa 22 :susan 23})
+    (println names)
+    (println (get names :anson))
+    (def lisaAge (get names :lisa))
+    (println (str "Lisa's Age: " lisaAge))
+    ; We can also retrieve values from Maps like this:
+    (println (:anson names)) ; Use the key itself as a function instead of using the 'get' function.
+    (println (:lisa names))
+    (println (:susan names))
 )
 ; lein run -m clj-test.mylists
