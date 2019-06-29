2f6a86ec90924f35208d30197e8cd6579a7f5f8d
ansonfoong <afoong@albany.edu>
2019-06-29 18:09:00 -0400

    use (count) function to get the count of a collection

--- src/clj_test/mylists.clj
+++ src/clj_test/mylists.clj
@@ -11,8 +11,13 @@
     ; Vectors
     ; Vectors can be accessed by indices, lists cannot.
     ; Both vectors and lists are considered collections in Clojure
+    ; All collections are immutable (they never change) and persistent.
+    ; Note: cons function does NOT mutate the collection.
     (def nameVector ["Joshua" "Seth" "Michael" "Logan" "Will"])
     (println nameVector)
     (println (str "Last name in Vector: " (last nameVector)))
+    ; Get the Count of the nameVector Collection
+    (def sizeOfNames (count nameVector))
+    (println (str "Total Names in Vector nameVector: " sizeOfNames))
 )
 ; lein run -m clj-test.mylists
