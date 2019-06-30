cb3cfe9355d3f0775699d1362a65e73f3be9dd1b
ansonfoong <afoong@albany.edu>
2019-06-30 12:55:04 -0400

    Using recursion, along with (first) and (rest) to map a vector to a new vector of sentences

--- src/clj_test/recur.clj
+++ src/clj_test/recur.clj
@@ -12,9 +12,19 @@
     )
 )
 
+(defn iam [listOfAdjectives output]
+    (if (empty? listOfAdjectives) output
+        (iam (rest listOfAdjectives) (conj output (str "I am " (first listOfAdjectives))))
+    )
+)
+
 (defn -main "Main" [& args]
     (traverse [1 2 3]) ; Traverse a Vector
     (traverse '(4 5 6)) ; Traverse a List
     (traverse {:anson '(1 2 3) :josh '(4 5 6) :zach '(7 8 9)}) ; Traverse a Map
     (traverse #{:blue :yellow :green :red}) ; Traverse a Set.
+
+    (def adjectives ["Tall" "Beautiful" "Buff" "Cool"])
+    (def myOutput (iam adjectives []))
+    (traverse myOutput)
 )
