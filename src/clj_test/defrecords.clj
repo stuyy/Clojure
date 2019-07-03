e49b2884ff077c64dc72d096d89cd41c02d1ba25
ansonfoong <afoong@albany.edu>
2019-07-03 15:54:10 -0400

    flip-left and flip-right only flip 1 page

--- src/clj_test/defrecords.clj
+++ src/clj_test/defrecords.clj
@@ -3,6 +3,8 @@
 (defprotocol UpdatePage
   (flip-right-amt [this amt])
   (flip-left-amt [this amt])
+  (flip-left [this])
+  (flip-right [this])
 )
 (defrecord Book [title author published current pages]
   UpdatePage
@@ -15,6 +17,12 @@
     (if (>= (- @(.-current this) amt) 1)
       (reset! (.-current this) (- @(.-current this) amt))
       (reset! (.-current this) 1)))
+  
+  (flip-left [this]
+    (when (> @(.-current this) 1) (swap! (.-current this) dec)))
+  
+  (flip-right [this]
+    (when (< @(.-current this) (.-pages this)) (swap! (.-current this) inc)))
 )
 (def harry-potter (Book. "Harry Potter and the Deathly Hallows" "J.K. Rowling" "2007" (atom 1) 759))
 (def percy-jackson (Book. "Percy Jackson and the Olympians: The Lightning Thief" "Rick Riordan" "2005" (atom 1) 377))
@@ -29,4 +37,9 @@
   (println (str "Current Page " @(.-current harry-potter)))
   (flip-left-amt harry-potter 22)
   (println (str "Current Page " @(:current harry-potter)))
+
+  (dotimes [n 4] (flip-right harry-potter))
+  (flip-left harry-potter)
+  (flip-left harry-potter)
+  (println @(:current harry-potter))
 )
