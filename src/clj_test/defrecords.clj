645fea7e1b70a4dce4b1bd14126cc7e4106d5f6b
ansonfoong <afoong@albany.edu>
2019-07-03 15:50:47 -0400

    flip-left-amt is the inverse of flip-right-amt

--- src/clj_test/defrecords.clj
+++ src/clj_test/defrecords.clj
@@ -1,16 +1,21 @@
 (ns clj-test.defrecords (:gen-class))
 
 (defprotocol UpdatePage
-  (flip-page-right [this amt])
+  (flip-right-amt [this amt])
+  (flip-left-amt [this amt])
 )
 (defrecord Book [title author published current pages]
   UpdatePage
-  (flip-page-right [this amt]
-    (println (.-pages this))
+  (flip-right-amt [this amt]
     (if (> (+ @(.-current this) amt) (.-pages this))
       (reset! (.-current this) (.-current pages))
-      (reset! (.-current this) (+ @(.-current this) amt))
-    )))
+      (reset! (.-current this) (+ @(.-current this) amt))))
+
+  (flip-left-amt [this amt]
+    (if (>= (- @(.-current this) amt) 1)
+      (reset! (.-current this) (- @(.-current this) amt))
+      (reset! (.-current this) 1)))
+)
 (def harry-potter (Book. "Harry Potter and the Deathly Hallows" "J.K. Rowling" "2007" (atom 1) 759))
 (def percy-jackson (Book. "Percy Jackson and the Olympians: The Lightning Thief" "Rick Riordan" "2005" (atom 1) 377))
 
@@ -18,6 +23,10 @@
   (println (class harry-potter))
   (println (class percy-jackson))
   
-  (flip-page-right harry-potter 20)
+  (flip-right-amt harry-potter 20)
   (println (format "Current Page for %s is %d" (.-title harry-potter) @(.-current harry-potter)))
+  (flip-left-amt harry-potter 15)
+  (println (str "Current Page " @(.-current harry-potter)))
+  (flip-left-amt harry-potter 22)
+  (println (str "Current Page " @(:current harry-potter)))
 )
